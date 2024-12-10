11e82fca59e3b8b4c34c0a7ae3d8f82efa95d81f
HectoraGarcia1711 <garciaalvarezhector36@gmail.com>
2024-12-10 05:47:20 -0600

    PRUEBA

--- src/Layouts/Login/Login.tsx
+++ src/Layouts/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom'; // Importamos Link
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Este componente será el contenedor principal donde se mostrará el formulario o cualquier contenido
@@ -16,72 +16,71 @@ export default function Login() {
               src="/src/assets/imgs/Blanco 2.svg" // Asegúrate de que la ruta sea correcta
               alt="Logo"
               className="img-fluid rounded-circle shadow mb-4"
-              style={{ maxWidth: "250px" }}
+              style={{ maxWidth: "300px" }}
             />
           </div>
 
           {/* Datos obligatorios */}
           <div className="row">
-            <div className="col-12 text-start mb-3">
-              <p>
-                <strong>Datos obligatorios *</strong>
-              </p>
-            </div>
+          <div className="col-12 text-start mb-3">
+             <strong><h6>Datos obligatorios *</h6></strong>
           </div>
-
-          {/* Formulario de Login */}
-          <form>
-            {/* Correo Electrónico */}
+          </div>
             {/* Formulario de Login */}
           <form>
             {/* Correo Electrónico */}
             <div className="mb-3">
               <label htmlFor="email" className="form-label text-start">
-                Correo Electrónico *
               </label>
               <input
                 type="email"
                 className="form-control mx-auto"
                 id="email"
-                placeholder="Correo Electrónico"
+                placeholder="Correo Electrónico *"
                 required
-                style={{ maxWidth: "400px" }}  // Limita el ancho de los campos
+                style={{ maxWidth: "450px" }}  // Limita el ancho de los campos
               />
             </div>
 
             {/* Contraseña */}
             <div className="mb-3">
               <label htmlFor="password" className="form-label text-start">
-                Contraseña *
               </label>
               <input
                 type="password"
                 className="form-control mx-auto"
                 id="password"
-                placeholder="Contraseña"
+                placeholder="Contraseña *"
                 required
-                style={{ maxWidth: "400px" }}  // Limita el ancho de los campos
+                style={{ maxWidth: "450px" }}  // Limita el ancho de los campos
               />
             </div>
+            <br></br>
 
             {/* Botón de Enviar */}
             <div className="d-flex justify-content-center">
-              <button type="submit" className="btn btn-primary">
+              {/* Link que redirige a la página de inicio */}
+              <Link to="/home" className="btn btn-primary">
                 Iniciar sesión
-              </button>
+              </Link>
             </div>
           </form>
         </div>
+        <br></br>
 
         {/* Imagen al lado derecho */}
-        <div className="col-md-6 d-flex justify-content-center align-items-center">
-          <img
-            alt={"Login Image"}
-            src="/src/assets/imgs/negro.svg" // Ruta de la imagen a la derecha
-            className="img-fluid" // Hace que la imagen se ajuste bien en la pantalla
-            style={{ width: "100%", height: "100%", objectFit: "cover" }}
-          />
-        </div>
+        <div className="col-md-6 d-flex justify-content-center align-items-center p-0 m-0">
+        <img
+    alt="Login Image"
+    src="/src/assets/imgs/negro.svg" // Ruta de la imagen
+    className="img-fluid" // Hace que la imagen se ajuste bien en la pantalla
+    style={{
+      width: "100%",           // La imagen ocupará el 100% del ancho del contenedor
+      height: "100vh",         // La imagen ocupará el 100% de la altura de la pantalla
+      objectFit: "cover",      // Mantiene el aspecto de la imagen, recortándola si es necesario
+    }}
+  />
+</div>
       </div>
     </div>
   );
