91569a6ea5ac0c879106d811cfe7530363d6203b
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-12-10 04:54:26 -0600

    agergando seccion de navbar

--- src/Layouts/Home/Home.tsx
+++ src/Layouts/Home/Home.tsx
@@ -1,15 +1,24 @@
-// import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { Navbar } from '../../Components/Navbar'
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../../Components/Navbar";
+// import Navbar from "../../Components/Navbar"
 
 export default function Home() {
   return (
-    <div>
-      <h2>Bienvenido</h2>
-        <Navbar/>
-      <main>
-        <Outlet/>
+    <div className="d-flex" style={{ height: "100vh", width: "100%" }}>
+      {/* Sidebar */}
+      <Navbar />
+
+      {/* Contenido principal */}
+      <main
+        className="flex-grow-1 bg-light"
+        style={{
+          marginLeft: "60px", // Ajusta este valor al ancho inicial del sidebar
+          padding: "20px",
+        }}
+      >
+        <h2>Bienvenido</h2>
+        <Outlet />
       </main>
     </div>
-  )
+  );
 }
